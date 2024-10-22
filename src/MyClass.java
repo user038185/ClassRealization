3f7e02131418a5d5941e870b608d762183e40d14
Vika <@user038185>
2024-10-22 23:51:01 +0300

    Реализовала класс MyClass и написала для него конструктор

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    private int A;
+    private String B;
+    private double C;
+
+    public MyClass(int A, String B, double C) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+    }
+}
