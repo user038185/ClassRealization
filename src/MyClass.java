d88e3ffeb7292f6419a7c3be06993ab303472877
Vika <@user038185>
2024-10-22 23:52:00 +0300

    Добавила методы set и get и вывод полей класса

--- src/MyClass.java
+++ src/MyClass.java
@@ -8,4 +8,30 @@ public class MyClass {
         this.B = B;
         this.C = C;
     }
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int A) {
+        this.A = A;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String B) {
+        this.B = B;
+    }
+    public double getC() {
+        return C;
+    }
+
+    public void setC(double C) {
+        this.C = C;
+    }
+
+    public void printMyClass() {
+        System.out.println("A: " + A + ", B: " + B + ", C: " + C);
+    }
 }
